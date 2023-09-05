b3ac05603a1226b3be9db9d8b8e179751569c5e9
ED HO <luciferomax002@gmail.com>
2023-09-05 20:10:06 +0900

    update

--- MOVE_FILE/COPY_DELETE_FOR.bat
+++ MOVE_FILE/COPY_DELETE_FOR.bat
@@ -4,22 +4,33 @@ set "Path1=D:\work_space1"
 set "Path2=D:\work_space2"
 set "Path3=D:\work_space3"
 
-REM 設定今天日期字串 (yyyyMMdd)
+REM 今日の日付文字列を設定 (yyyyMMdd)
 set "todayDate=%DATE:/=%"
 
-REM 設定上週日期字串 (yyyyMMdd)，値為今天日期減 7 天
+REM 今日から7日前の日付文字列を設定 (yyyyMMdd)
 set "lastWeekDate=%date:~0,4%%date:~4,2%%date:~6,2%"
 set /a lastWeekDate=lastWeekDate-7
 
-REM 迴圈處理路徑一，取出 okta_ 開頭且 .csv 結尾的檔案
-for %%I in ("%Path1%\okta_*.csv") do (
+REM ファイルの存在を判定する
+if exist "%Path2%\flag.flg" (
+    del "%Path2%\flag.flg"
+    echo "delete flag.flg success。"
+) else (
+    echo 檔案不存在。
+)
+
+REM パス2をループ処理し、okta_ で始まり .csv で終わるファイルを取得する
+for %%I in ("%Path2%\okta_*.csv") do (
     set "fileName=%%~nxI"
     set "fileDate=!fileName:okta_=!"
     set "fileDate=!fileDate:.csv=!"
 
-    REM 判斷檔案中的日期 (yyyyMMdd) 是否小於上週日期字串
+    REM ファイル内の日付（yyyyMMdd形式）が先週の日付文字列より小さいかどうかを判定する
     if !fileDate! lss %lastWeekDate% (
-        echo %%I 的日期為 !fileDate!，小於上週日期 %lastWeekDate%，將被刪除。
+        echo %%Iの日付は !fileDate! で、先週の日付よりも古いため、削除されます。
         del "%%I"
     )
 )
+
+echo true > %Path2%\flag.flg
+
